e2ebd08b76d4dd7c4e730d11f277adbe4baa9c5e
KesThav <kesigan.thav@gmail.com>
2022-02-04 09:52:20 +0100

    the snake game

---
+++ src/SnakeGame.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class SnakeGame extends Application {
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+}
